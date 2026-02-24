fcec6b9c9cb436e7755d091cb1e2a65e9e0cc283
Your Name <your@email.com>
2026-02-24 19:11:33 +0530

    Initial commit - Backend files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('backend is running');
+});
+app.use(cors());
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
